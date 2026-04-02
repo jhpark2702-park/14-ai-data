13ba55140b8f207b26ea76145ad2f59bb5383a69
jhpark2702 <jhpark2702@naver.com>
2026-04-02 21:10:24 +0900

    20260402

--- 05_플라스크/app.py
+++ 05_플라스크/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template()
+    return render_template("index.html")
 
 @app.route("/hello")
 def hello():
@@ -15,4 +15,4 @@ def profile(userId):
     return f"{userId}\'s profile"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
